71287e9e57e8b5443d3365ce928ca325e2016ce2
DavidLiger <52631629+DavidLiger@users.noreply.github.com>
2022-08-04 09:54:58 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+
+echo '<!DOCTYPE html>
+ <html>
+   <head>
+      <meta charset="utf-8"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+      <title>David Liger - Folio Web</title>
+      <link rel="icon" type="image/ico" href="/../src/folio/assets/images/icon_site-small.png" />
+      <link type="text/css" rel="stylesheet" href="/../build/css/font.css">
+      <link type="text/css" rel="stylesheet" href="/../src/folio/css/screenLoader.css">
+      <link type="text/css" rel="stylesheet" href="/../src/folio/css/folio.css">
+      <link id="GLTFUI" type="text/css" rel="stylesheet" href="/../src/folio/css/GLTFUI.css">
+      <link type="text/css" rel="stylesheet" href="/../src/folio/css/modal.css">
+      <link id="modalHelperCSS" type="text/css" rel="stylesheet" href="../../../../build/MVCComponents/ViewComponent/ModalHelper/ModalHelper.css">
+      <link id="jsonFilterListCSS" type="text/css" rel="stylesheet" href="../../../../build/MVCComponents/ViewComponent/JsonFilterList/JsonFilterList.css">
+      <link id="timeStepperCSS" type="text/css" rel="stylesheet" href="../../../../build/MVCComponents/ViewComponent/TimeStepper/TimeStepper.css">
+      <link id="projectDisplayerCSS" type="text/css" rel="stylesheet" href="../../../../build/MVCComponents/ViewComponent/ProjectDisplayer/ProjectDisplayer.css">
+      <link id="carouselCSS" type="text/css" rel="stylesheet" href="../../../../build/MVCComponents/ViewComponent/Carousel/Carousel.css">
+      <link id="imageViewerCSS" type="text/css" rel="stylesheet" href="../../../../build/MVCComponents/ViewComponent/ImageViewer/ImageViewer.css">
+      <link id="contactFormCSS" type="text/css" rel="stylesheet" href="../../../../build/MVCComponents/ViewComponent/ContactForm/ContactForm.css">
+      <link rel="preconnect" href="https://www.google.com">
+      <link rel="preconnect" href="https://www.gstatic.com" crossorigin>
+      <script src="https://www.google.com/recaptcha/api.js"></script>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+      <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+      <script id="jQueryScriptMobile" src="//code.jquery.com/mobile/1.5.0-alpha.1/jquery.mobile-1.5.0-alpha.1.min.js"></script>
+      <script src="https://code.jquery.com/ui/1.13.0/jquery-ui.min.js" integrity="sha256-hlKLmzaRlE8SCJC1Kw8zoUbU8BxA+8kR3gseuKfMjxA=" crossorigin="anonymous"></script>
+      <script src="../build/lib/three.min.js"></script>
+      <script src="../build/lib/GLTFLoader.js"></script>
+      <script type="module" src="../src/folio/js/Index.js"></script>
+      <script src="https://www.youtube.com/player_api"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.14.305/pdf.min.js" integrity="sha512-dw+7hmxlGiOvY3mCnzrPT5yoUwN/MRjVgYV7HGXqsiXnZeqsw1H9n9lsnnPu4kL2nx2bnrjFcuWK+P3lshekwQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+      <script src="//mozilla.github.io/pdf.js/build/pdf.js"></script>
+   </head>
+   <body>
+    <div id="container"></div>
+   </body>
+ </html>'
+
+ ?>
